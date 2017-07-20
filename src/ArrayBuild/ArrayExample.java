59d359b3a72a7b21a903534ea72a74f1357c76de
rdnasim <rdislamnasim78@gmail.com>
2017-07-20 14:21:16 +0600

    Initial commit

---
+++ src/ArrayBuild/ArrayExample.java
@@ -0,0 +1,10 @@
+package ArrayBuild;
+
+public class ArrayExample {
+    public static void main(String args[]) {
+        int a[] = {33, 4, 5, 15};
+
+        for (int i = 0; i<a.length; i++)
+            System.out.println(a[i]);
+    }
+}
